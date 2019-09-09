6cbc4303e600f4581a90ae572a80697dee9484c5
Rohan Samavedam <42583450+rohansamavedam@users.noreply.github.com>
2019-09-08 20:46:22 -0700

    Variable and Data Types

---
+++ variables.py
@@ -0,0 +1,38 @@
+# A variable is a container for a value, which can be of various types
+
+'''
+This is a 
+multiline comment
+or docstring (used to define a functions purpose)
+can be single or double quotes
+'''
+
+"""
+VARIABLE RULES:
+  - Variable names are case sensitive (name and NAME are different variables)
+  - Must start with a letter or an underscore
+  - Can have numbers but can not start with one
+"""
+
+# x = 1             # int
+# y = 2.5           # float
+# name = 'Rohan'    # strings
+# is_cool = True    # bool
+
+# Multiple Assignments
+x, y, name, is_cool = (1, 2.5, 'Rohan', True)
+
+# Print
+print(x, y, name, is_cool)
+
+# Basic Math
+a = x + y
+
+# Casting
+x = str(x)
+y = int(y)
+
+# Check Type
+print(type(x))
+print(type(y))
+print(y)
